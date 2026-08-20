5bbc0f5f0c17f1599015f294d66d7e8f8b0db380
PranauvCse2006 <pranauvsps@gmail.com>
2026-08-20 07:04:49 +0530

    COMPLETED DocumentUploadForm.js

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name , setName]=useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Digital Document Vault System </h1>
+    <p>Securely store and manage your documents.</p>
+     <input
+                type="text"
+                onChange={(e) => setName(e.target.value)}
+            />
+    <h2>{name}</h2>
     </div>
   );
 }
---
+++ reactapp/src/components/DocumentUploadForm.js
@@ -0,0 +1,46 @@
+import {useState} from "react";
+function DocumentUploadForm(){
+    const [documentTitle, setDocumentTitle] = useState("");
+    const [category, setCategory] = useState("");
+    const [file, setFile] = useState(null);
+
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log(documentTitle);
+        console.log(category);
+        console.log(file);
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+            <input
+            type="text"
+            placeholer="Document Title"
+            value={documentTitle}
+            onChange={(e)=>setDocumentTitle(e.target.value)}
+            />
+
+            <input
+            type="text"
+            placeholder="Category"
+            value={category}
+            onChange={(e)=>setCategory(e.target.value)}
+            />
+
+            <input
+            type="file"
+            onChange={(e)=>setFile(e.target.file[0])}
+            />
+
+
+            <p>Title: {documentTitle}</p>
+            <p>Category: {category}</p>
+            <p>File: {file ? file.name:"No file selected"}</p>
+
+            <button type="submit">Upload</button>
+            </form>
+        </div>
+
+    )
+}
+export default DocumentUploadForm;
